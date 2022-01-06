4c732879e4de1b0091ed3a033bd21167468476ad
RejayMorada0 <rejaymorada0@gmail.com>
2022-01-06 13:52:31 +0800

    Add Files

--- pic/js/checking.js
+++ pic/js/checking.js
@@ -14,280 +14,146 @@ function details_hide_show() {
     }
 
 }
-var ffremarks = []
+
+// Reference: https://stackoverflow.com/questions/32383547/javascript-search-for-element-in-multidimensional-array
 
 
-var firstyearfirstsem = [
-    //FIRST YEAR - FIRST  SEMESTER
-    ['BET1-C', 'Orientation to BET, Seminars and Field Trips'],
-    ['CHEMGEN-C', ' General Chemistry (Lec)','',''],
-    ['CHEMGENL-C', ' General Chemistry (Lab)','',''],
-    ['CPET 1L-C', ' Program Logic and Formulation (Lab)','','']
-]
-
-var firstyearsecondsem = [
-    //FIRST YEAR - SECOND SEMESTER
-    ['ET1-C', 'Electrical Circuits (Lec)','',''],
-    ['ET1L-C', 'Electrical Circuits (Lab)','',''],
-    ['GEC1-C', 'GEC1-C','',''],
-    ['GEC4-C', 'Mathematics in the Modern World 1','','']
-]
-
-var secondyearfirstsem = [
+// student grades
+var students_grade = [
     //FIRST YEAR - FIRST AND SECOND SEMESTER
-    ['MATHA05-C', ' Pre-Calculus','',''],
-    ['NSTP1-C', ' National Service Training Program 1','',''],
-    ['PE1-C', ' Physical Fitness','',''],
-    ['CAD-C', ' Computer Aided Drafting','',''],
-]
 
-var secondyearsecondsem = [
-   
-    ['MATHA05-C', ' Pre-Calculus','',''],
-    ['NSTP1-C', ' National Service Training Program 1','',''],
-    ['PE1-C', ' Physical Fitness','',''],
-    ['CAD-C', ' Computer Aided Drafting','',''],
-]
+    ['BET1-C', 'Orientation to BET, Seminars and Field Trips', 'FIRST YEAR - FIRST SEMESTER'],
+    ['CHEMGEN-C', 'General Chemistry (Lec)', 'FIRST YEAR - FIRST SEMESTER'],
+    ['CHEMGENL-C', 'General Chemistry (Lab)', 'FIRST YEAR - FIRST SEMESTER'],
+    ['CPET 1L-C', 'Program Logic and Formulation (Lab)', 'FIRST YEAR - FIRST SEMESTER'],
+    ['ET1-C', 'Electrical Circuits (Lec)', 'FIRST YEAR - SECOND SEMESTER'],
+    ['ET1L-C', 'Electrical Circuits (Lab)', 'FIRST YEAR - SECOND SEMESTER'],
+    ['GEC1-C', 'Understanding the Self', 'FIRST YEAR - SECOND SEMESTER'],
+    ['GEC4-C', 'Mathematics in the Modern World 1', 'FIRST YEAR - SECOND SEMESTER'],
 
-var thirdyearfirstsem = [
-    
-    ['MATHA05-C', ' Pre-Calculus','',''],
-    ['NSTP1-C', ' National Service Training Program 1','',''],
-    ['PE1-C', ' Physical Fitness','',''],
-    ['CAD-C', ' Computer Aided Drafting','',''],
-
-]
-
-var thirdyearsecondsem = [
-    
-    ['MATHA05-C', ' Pre-Calculus','',''],
-    ['NSTP1-C', ' National Service Training Program 1','',''],
-    ['PE1-C', ' Physical Fitness','',''],
-    ['CAD-C', ' Computer Aided Drafting','',''],
-
-]
-
-var fourthyearfirstsem = [
-    
-    ['MATHA05-C', ' Pre-Calculus','',''],
-    ['NSTP1-C', ' National Service Training Program 1','',''],
-    ['PE1-C', ' Physical Fitness','',''],
-    ['CAD-C', ' Computer Aided Drafting','',''],
-]
-
-var fourthyearsecondsem = [
-    
-    ['MATHA05-C', ' Pre-Calculus','',''],
-    ['NSTP1-C', ' National Service Training Program 1','',''],
-    ['PE1-C', ' Physical Fitness','',''],
-    ['CAD-C', ' Computer Aided Drafting','',''],
-    
     //SECOND YEAR - FIRST AND SECOND SEMESTER
-    //['MATHA05-C', 'Pre-Calculus', 'SECOND YEAR - FIRST SEMESTER'],
-    //['NSTP1-C', 'National Service Training Program 1', 'SECOND YEAR - FIRST SEMESTER'],
-    //['PE1-C', 'Physical Fitness', 'SECOND YEAR - FIRST SEMESTER'],
-   // ['CAD-C', 'Computer Aided Drafting', 'SECOND YEAR - FIRST SEMESTER'],
-  //  ['CHET-C', 'Chemistry for Engineering Technologists', 'SECOND YEAR - SECOND SEMESTER'],
-    //['CHETL-C', 'Chemistry for Engineering Technologists(Lab)', 'SECOND YEAR - SECOND SEMESTER'],
-   // ['CPET2L-C', 'Object Oriented Programming 2(Lab)', 'SECOND YEAR - SECOND SEMESTER'],
-   // ['EST1-C', 'Electronics 1(Lec)', 'SECOND YEAR - SECOND SEMESTER']
-    //THIRD YEAR - FIRST AND SECOND SEMESTER
+    ['MATHA05-C', 'Pre-Calculus', 'SECOND YEAR - FIRST SEMESTER'],
+    ['NSTP1-C', 'National Service Training Program 1', 'SECOND YEAR - FIRST SEMESTER'],
+    ['PE1-C', 'Physical Fitness', 'SECOND YEAR - FIRST SEMESTER'],
+    ['CAD-C', 'Computer Aided Drafting', 'SECOND YEAR - FIRST SEMESTER'],
+    ['CHET-C', 'Chemistry for Engineering Technologists', 'SECOND YEAR - SECOND SEMESTER'],
+    ['CHETL-C', 'Chemistry for Engineering Technologists(Lab)', 'SECOND YEAR - SECOND SEMESTER'],
+    ['CPET2L-C', 'Object Oriented Programming 2(Lab)', 'SECOND YEAR - SECOND SEMESTER'],
+    ['EST1-C', 'Electronics 1(Lec)', 'SECOND YEAR - SECOND SEMESTER'],
 
+    //THIRD YEAR - FIRST AND SECOND SEMESTER
+    ['EST1L-C', 'Electronics 1 Lab', 'THIRD YEAR - FIRST SEMESTER'],
+    ['GEC2-C', 'Readings in Philippine History', 'THIRD YEAR - SECOND SEMESTER'],
+    ['GEC5-C', 'Purposive Communication', 'THIRD YEAR - FIRST SEMESTER'],
+    ['GEC7-C', 'Science, Technology, and Society', 'THIRD YEAR - SECOND SEMESTER'],
+    ['MATHA13-C', 'Differential Calculus', 'THIRD YEAR - FIRST SEMESTER'],
+    ['NSTP2-C', 'National Service Training Program 2', 'THIRD YEAR - SECOND SEMESTER'],
+    ['PE2-C', 'Rhythmic Activities', 'THIRD YEAR - FIRST SEMESTER'],
+    ['CPET3-C', 'Logic Circuits and Switching Theory Lec.', 'THIRD YEAR - SECOND SEMESTER'],
 
     //FOURTH YEAR - FIRST AND SECOND SEMESTER
+    ['CPET3L-C', 'Logic Circuits and Switching Theory Lab.', 'FOURTH YEAR - FIRST SEMESTER'],
+    ['CPET4-C', 'Data and Digital Communcations', 'FOURTH YEAR - SECOND SEMESTER'],
+    ['CPET5L-C', 'Data Structures and Algorithms Lab.', 'FOURTH YEAR - FIRST SEMESTER'],
+    ['CPET6L-C', 'Introduction to Hardware Description Language', 'FOURTH YEAR - SECOND SEMESTER'],
+    ['GEC8-C', 'Ethics', 'FOURTH YEAR - FIRST SEMESTER'],
+    ['GEM14-C', 'Life and Works of Rizal', 'FOURTH YEAR - SECOND SEMESTER'],
+    ['MATHA23-C', 'Integral Calculus', 'FOURTH YEAR - FIRST SEMESTER'],
+    ['PE3-C', 'Individual and Dual Sports', 'FOURTH YEAR - SECOND SEMESTER']
+]
 
-];
-//print coetsubjects in console
-//console.log(firstsubjects)
 
 
-// funtions for displaying of list in table
-//reference: https://stackoverflow.com/questions/15164655/generate-html-table-from-2d-javascript-array
-function createTable1a(tableData) {
-    var table = document.getElementById('1a');
-    var row = {};
-    var cell = {};
+function displayTable() {
 
-    tableData.forEach(function(rowData) {
-        row = table.insertRow(-1); // [-1] for last position in Safari
-        rowData.forEach(function(cellData) {
-            cell = row.insertCell();
-            cell.textContent = cellData;
-        });
-    });
-    //document.body.appendChild(table);
+    var table = document.getElementById('allsub');
+    var len = students_grade.length;
+    let x = ''
+    for (let i = 0; i < len; i++) {
+        // x += students_grade[i][1];
+
+        //add row
+        var row = table.insertRow();
+        //add cells
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+
+        cell1.innerHTML = students_grade[i][0];
+        cell2.innerHTML = students_grade[i][1];
+        cell3.innerHTML = students_grade[i][2];
+        cell4.innerHTML = students_grade[i][3];
+        cell5.innerHTML = students_grade[i][4];
+
+        //add onclick
+        document.getElementsByTagName("tr")[i + 1].setAttribute("onclick", "rowGetter(this)");
+
+    }
+
 }
 
-createTable1a(firstyearfirstsem);
-
-function createTable1b(tableData) {
-    var table = document.getElementById('1b');
-    var row = {};
-    var cell = {};
-
-    tableData.forEach(function(rowData) {
-        row = table.insertRow(-1); // [-1] for last position in Safari
-        rowData.forEach(function(cellData) {
-            cell = row.insertCell();
-            cell.textContent = cellData;
-        });
-    });
-    //document.body.appendChild(table);
-}
-
-createTable1b(firstyearsecondsem);
-
-function createTable2a(tableData) {
-    var table = document.getElementById('2a');
-    var row = {};
-    var cell = {};
-
-    tableData.forEach(function(rowData) {
-        row = table.insertRow(-1); // [-1] for last position in Safari
-        rowData.forEach(function(cellData) {
-            cell = row.insertCell();
-            cell.textContent = cellData;
-        });
-    });
-    //document.body.appendChild(table);
-}
-
-createTable2a(secondyearfirstsem);
-
-function createTable2b(tableData) {
-    var table = document.getElementById('2b');
-    var row = {};
-    var cell = {};
-
-    tableData.forEach(function(rowData) {
-        row = table.insertRow(-1); // [-1] for last position in Safari
-        rowData.forEach(function(cellData) {
-            cell = row.insertCell();
-            cell.textContent = cellData;
-        });
-    });
-    //document.body.appendChild(table);
-}
-
-createTable2b(secondyearsecondsem);
+displayTable();
 
 
-
-function createTable3a(tableData) {
-    var table = document.getElementById('3a');
-    var row = {};
-    var cell = {};
-
-    tableData.forEach(function(rowData) {
-        row = table.insertRow(-1); // [-1] for last position in Safari
-        rowData.forEach(function(cellData) {
-            cell = row.insertCell();
-            cell.textContent = cellData;
-        });
-    });
-    //document.body.appendChild(table);
-}
-
-createTable3a(thirdyearfirstsem);
-
-function createTable3b(tableData) {
-    var table = document.getElementById('3b');
-    var row = {};
-    var cell = {};
-
-    tableData.forEach(function(rowData) {
-        row = table.insertRow(-1); // [-1] for last position in Safari
-        rowData.forEach(function(cellData) {
-            cell = row.insertCell();
-            cell.textContent = cellData;
-        });
-    });
-    //document.body.appendChild(table);
-}
-
-createTable3b(thirdyearsecondsem);
-
-function createTable4a(tableData) {
-    var table = document.getElementById('4a');
-    var row = {};
-    var cell = {};
-
-    tableData.forEach(function(rowData) {
-        row = table.insertRow(-1); // [-1] for last position in Safari
-        rowData.forEach(function(cellData) {
-            cell = row.insertCell();
-            cell.textContent = cellData;
-        });
-    });
-    //document.body.appendChild(table);
-}
-
-createTable4a(fourthyearfirstsem);
-
-function createTable4b(tableData) {
-    var table = document.getElementById('4b');
-    var row = {};
-    var cell = {};
-
-    tableData.forEach(function(rowData) {
-        row = table.insertRow(-1); // [-1] for last position in Safari
-        rowData.forEach(function(cellData) {
-            cell = row.insertCell();
-            cell.textContent = cellData;
-        });
-    });
-    //document.body.appendChild(table);
-}
-
-createTable4b(fourthyearsecondsem);
-
-
-
-//adding remakrs
 
 function addremarks() {
+    var subcode = document.forms["addremarksform"]['validationTooltip01'].value;
+    var remark = document.forms["addremarksform"]['validationTooltip02'].value;
 
-    var subjectcode = document.forms["addremarksform"]['validationTooltip01'].value;
-    var remarks = document.forms["addremarksform"]['validationTooltip02'].value;
-    outputText = "";
-    //window.alert(subjectcode)
+    var outputText = ""
 
-    for (var i = 0, len = firstyearfirstsem.length; i < len; i++) {
-        if (firstyearfirstsem[i][0] === subjectcode) {
-            outputText = "Remarks Added";
-            //coetsubjects.splice(i, 1);
-            // funtions for displaying and refreshing of list in table
-            //document.getElementById("1a").innerHTML = "";
-            //createTable1a(firstyearfirstsem);
-            //outputText = "Remarks added";
-            break; // need this break to prevent multiple results of the same array
+    //find the subject code and update the remark element
+    for (var i = 0, len = students_grade.length; i < len; i++) {
+        w = students_grade[i][0]
+        x = students_grade[i][1]
+        y = students_grade[i][2]
+        z = students_grade[i][3]
+        if (w == subcode) {
+            //update the matrix
+            students_grade[i] = [w, x, y, z, remark]
+
+            outputText = "Successfully Update the Remark"
         }
     }
-    if (subjectcode == "") {
-        outputText = "Please enter subject code"
-    } else if (outputText == "") {
-        outputText = "Subject not found";
+    if (outputText == "") {
+        outputText = "Subject Code Not Existing"
     }
-    // output the result
-    window.alert(outputText);
-
-
-   
-    
-    
-
-
-    
-    //outputText = "";
-
-    // search each cell for the query string
-    // source = https://stackoverflow.com/questions/8809425/search-multi-dimensional-array-javascript
-   
-        
-        // conditions or handling errors
-        //if (outputText == "") {
-        //    outputText = "Bulaga";
-       // }
-        // output the result
-       // window.alert(outputText);
+    //refresh table
+    document.getElementById("allsub").innerHTML = "";
+    displayTable()
+    window.alert(outputText)
 }
+
+
+
+/*
+
+function submitToAdmin() {
+
+    if (confirm("Are you sure you want to Submitted it? Once you Submit it you can not change it for good.") == true) {
+        let y = ''
+        students_grade.push([e]);
+        for (var i = 0, len = students_grade.length; i < len; i++) {
+            x = students_grade[i];
+            if (e == x) {
+                for (var j = 0, len = coetsubjects.length; j < len; j++) {
+                    y += coetsubjects[j];
+                    //update the matrix
+                    students_grade[i] = [e, y];
+                }
+            }
+
+        }
+        document.getElementById("validationTooltip01").disabled = true;
+        document.getElementById("validationTooltip02").disabled = true;
+        document.getElementById("updategrade").disabled = true;
+        document.getElementById("submitbtn").disabled = true;
+        window.alert(students_grade);
+    } else {
+        text = "You canceled!";
+    }
+}
+
+*/
